637a1962555ca62520a53adfff0c32861e704af4
Ziyodullo <90693543+ScorpiRui@users.noreply.github.com>
2024-08-22 14:05:05 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -4,6 +4,6 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = "6718810556:AAGvwFNmvdQmzu57cbS_7HEwIWIt5_VoI-o"  # Bot toekn
-ADMINS = [1135712336] # admin ID
+BOT_TOKEN = "YOUR_TOKEN"  # Bot toekn
+ADMINS = [] # admin ID
 
